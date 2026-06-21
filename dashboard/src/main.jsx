dbecf7d709a293b401013c9b3721d45e0b878535
dmwsec <dmwsecdevop@gmail.com>
2026-06-21 13:13:55 +0530

    style(ui): load integration config styles

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppRedesign from './AppRedesign.jsx'
 import './index.css'
+import './integrations.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
